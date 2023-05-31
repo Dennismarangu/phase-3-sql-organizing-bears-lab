7e547dd8c7766131d356e94c7eb7b8999d7fac3c
Dennismarangu <dennismutumamarangu@gmail.com>
2023-05-31 11:43:13 +0300

    Updated assignment

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (1, 'Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (2, 'Rowdy', 3, 'M', 'Black', 'Energetic', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (3, 'Tabitha', 7, 'F', 'Gray', 'Calm', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (4, 'Sergeant Brown', 6, 'M', 'Brown', 'Strict', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (5, 'Melissa', 4, 'F', 'Brown', 'Playful', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (6, 'Grinch', 2, 'M', 'Green', 'Grumpy', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (7, 'Wendy', 8, 'F', 'White', 'Friendly', 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (8, 'unnamed', NULL, NULL, NULL, NULL, NULL);
